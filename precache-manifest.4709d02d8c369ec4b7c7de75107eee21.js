fb0d82957aab0391dec1db18e7441221a04440b4
p-racoon <prasham9.ash@gmail.com>
2019-09-04 10:13:21 +0530

    Updates

---
+++ precache-manifest.4709d02d8c369ec4b7c7de75107eee21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf7b2dfe22a62daac6d7ad013a8dad9",
+    "url": "/ubuntu-portfolio-v2/index.html"
+  },
+  {
+    "revision": "bf12e838ed2a9488988b",
+    "url": "/ubuntu-portfolio-v2/static/css/main.540a3281.chunk.css"
+  },
+  {
+    "revision": "1a7766874dc7ed4b458f",
+    "url": "/ubuntu-portfolio-v2/static/js/2.7d77ce3e.chunk.js"
+  },
+  {
+    "revision": "bf12e838ed2a9488988b",
+    "url": "/ubuntu-portfolio-v2/static/js/main.903917c8.chunk.js"
+  },
+  {
+    "revision": "b000abf517c9a4ecd2aa",
+    "url": "/ubuntu-portfolio-v2/static/js/runtime~main.02a49cdb.js"
+  }
+]);
