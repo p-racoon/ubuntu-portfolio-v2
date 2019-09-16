6b76b8744a4c975af03ea195682ec636dca60878
p-racoon <prasham9.ash@gmail.com>
2019-09-16 11:04:04 +0530

    add Hamburger Icon

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./TopBar.css";
 
 export default function TopBar() {
-  return <div className="topBar">
-      <div className="leftBarContent">Prasham's Portfolio</div>
+  return (
+    <div className="topBar">
+      <div className="leftBarContent">
+        <i className="fas fa-bars hamburgerIcon" />
+        <span>Prasham's Portfolio</span>
+      </div>
       <div className="centeredBarContent">Sep 7 09:19</div>
       <div className="rightBarContent">Icons come here</div>
-  </div>;
+    </div>
+  );
 }
