09467952df71347d4328891a1a6439f9b4fa431d
p-racoon <prasham9.ash@gmail.com>
2019-09-16 11:52:04 +0530

    add power button

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./TopBar.css";
 
 export default function TopBar() {
@@ -14,17 +15,20 @@ export default function TopBar() {
       <div className="centeredBarContent">Sep 7 09:19</div>
       <div className="rightBarContent">
         <a href="https://linkedin.com/in/prashesh09/" target="_blank">
-          <i class="fab fa-linkedin-in"></i>
+          <i className="fab fa-linkedin-in"></i>
         </a>
         <a href="https://github.com/p-racoon" target="_blank">
-          <i class="fab fa-github"></i>
+          <i className="fab fa-github"></i>
         </a>
         <a href="https://medium.com/@prasham9.ash" target="_blank">
-          <i class="fab fa-medium-m"></i>
+          <i className="fab fa-medium-m"></i>
         </a>
         <a href="https://twitter.com/Prasham09" target="_blank">
-          <i class="fab fa-twitter"></i>
+          <i className="fab fa-twitter"></i>
         </a>
+        <Link to="/">
+          <img src="/images/icons/system/system-shutdown-symbolic.svg"></img>
+        </Link>
       </div>
     </div>
   );
