2a49c32daf17fe8a649d1114cc3aab2d3e76928b
p-racoon <prasham9.ash@gmail.com>
2019-09-16 09:51:47 +0530

    change Activities-> Prasham's Portfolio

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import "./TopBar.css";
 
 export default function TopBar() {
   return <div className="topBar">
-      <div className="leftBarContent">Activities</div>
+      <div className="leftBarContent">Prasham's Portfolio</div>
       <div className="centeredBarContent">Sep 7 09:19</div>
       <div className="rightBarContent">Icons come here</div>
   </div>;
