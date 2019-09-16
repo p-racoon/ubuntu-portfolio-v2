8b2c54832894438c39438f656e4d33a5de8ad803
p-racoon <prasham9.ash@gmail.com>
2019-09-16 11:32:12 +0530

    add Social Links

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -12,7 +12,20 @@ export default function TopBar() {
         <span>Prasham's Portfolio</span>
       </div>
       <div className="centeredBarContent">Sep 7 09:19</div>
-      <div className="rightBarContent">Icons come here</div>
+      <div className="rightBarContent">
+        <a href="https://linkedin.com/in/prashesh09/" target="_blank">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://github.com/p-racoon" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://medium.com/@prasham9.ash" target="_blank">
+          <i class="fab fa-medium-m"></i>
+        </a>
+        <a href="https://twitter.com/Prasham09" target="_blank">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </div>
     </div>
   );
 }
